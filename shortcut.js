df561a122a68b33d73256a75594c4dfa241a4689
mitchell-rl <47991216+mitchell-rl@users.noreply.github.com>
2021-04-25 23:02:36 -0700

    Attempt 1 at no input case logic

--- shortcut.js
+++ shortcut.js
@@ -42,32 +42,43 @@ function findLabels() {
   for (label of labels) {
     //Pick out Storefront URL labels
     labelTitle = label.innerText.toLowerCase();
-
     if (labelTitle === "storefront url") {
       //select it's input field
       let inputField = label.nextElementSibling;
-      console.log(inputField);
       //Validate whether shopURL is a "myshopify.com" URL
       if (inputField.value.toString().includes(".myshopify.com")) {
         // success type
         let shopURL = inputField.value.toString();
-        console.log(`Valid Storefront URL found: ${shopURL}`);
+        console.log(
+          `%cShortcuts– Valid Storefront URL found: ${shopURL}`,
+          "color:green;"
+        );
         buttonsArray.push(
           new ShortcutButton(shopURL, inputField.parentElement, "success")
         );
-      } else {
-        // failure type
+      } else if (inputField.value.toString() == "") {
+        // failure type 1 (no input)
         let shopURL = inputField.value.toString();
         console.log(
-          `Invalid Storefront URL found: ${shopURL}. Please use 'storename.myshopify.com' in Storefront URL ticket field.`
+          `%cShortcuts– No Storefront URL found. Please use 'storename.myshopify.com' in Storefront URL ticket field.`,
+          "color:red;"
         );
         buttonsArray.push(
-          new ShortcutButton(shopURL, inputField.parentElement, "failed")
+          new ShortcutButton(shopURL, inputField.parentElement, "failed-1")
+        );
+      } else {
+        // failure type 2 (wrong input)
+        let shopURL = inputField.value.toString();
+        console.log(
+          `%cShortcuts– Invalid Storefront URL found: ${shopURL}. Please use 'storename.myshopify.com' in Storefront URL ticket field.`,
+          "color:red;"
+        );
+        buttonsArray.push(
+          new ShortcutButton(shopURL, inputField.parentElement, "failed-2")
         );
       }
     }
   }
-  console.dir(buttonsArray);
 }
 
 /* 
@@ -86,21 +97,48 @@ function buildButtons() {
     let type = button.type;
 
     function build(btnURL, btnParent, btnType) {
-      // 1. Create the button
       let partnerButton = document.createElement("button");
-      partnerButton.classList.add("shortcut__button--success");
-      partnerButton.innerHTML = "&#x1F50E;";
-      // 2. Append to parent
-      parent.appendChild(partnerButton);
-      // 3. Add event handler
-      partnerButton.addEventListener("click", function () {
-        launchSearch(value);
-      });
-      console.dir(partnerButton);
+
+      if (btnType == "success") {
+        // 1. Create the button
+        partnerButton.classList.add("shortcut__button--success");
+        partnerButton.innerHTML = "&#x1F50E;";
+        partnerButton.title = "Click to search partner dashboard.";
+        // 2. Append to parent
+        parent.appendChild(partnerButton);
+        // 3. Add event handler
+        partnerButton.addEventListener("click", function () {
+          launchSearch(value);
+        });
+      } else if (btnType == "failed-1") {
+        let inputBox = parent.childNodes[1];
+        // 1. Create the button
+        partnerButton.classList.add("shortcut__button--failure");
+        partnerButton.innerHTML = "&#9432;";
+        partnerButton.title =
+          "Invalid link. Please use storename.myshopify.com instead.";
+        // 2. Append to parent
+        parent.appendChild(partnerButton);
+        // 3. Add event handler
+        console.dir(inputBox);
+        inputBox.addEventListener("change", loadCheck()); // NOTE This is not working
+      } else {
+        // 1. Create the button
+        partnerButton.classList.add("shortcut__button--failure");
+        partnerButton.innerHTML = "&#9432;";
+        partnerButton.title =
+          "Invalid link. Please use storename.myshopify.com instead.";
+        // 2. Append to parent
+        parent.appendChild(partnerButton);
+        // 3. Add event handler
+        partnerButton.addEventListener("click", function () {
+          launchSearch(value);
+        });
+      }
     }
+
     //Check whether there's already a button here and only add one if not.
     if (parent.childNodes.length <= 2) {
-      console.log("building button");
       build(value, parent, type);
     }
   }
@@ -108,6 +146,10 @@ function buildButtons() {
 
 /* set timer to check for labels that exist on the page, and then run the program */
 function loadCheck() {
+  // initialize arrays - NOTE This is not enough to unload current buttons 🤔
+  labels = [];
+  buttonsArray = [];
+
   let checkForLabels = setInterval(function () {
     findLabels();
     buildButtons();
@@ -117,11 +159,11 @@ function loadCheck() {
   }, 1000);
 }
 
-loadCheck();
-
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message.includes("Tab:")) {
-    console.log(`${request.message}`);
+    console.log(`%cShortcuts– background: ${request.message}`, "color:white;");
     loadCheck();
   }
 });
+
+loadCheck();
